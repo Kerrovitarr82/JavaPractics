409796bf5a891203acb58f166eb887d40519757a
Kerrovitarr82 <kokosk29@gmail.com>
2021-09-08 13:38:42 +0300

    Упр 5 в 3ей практике

---
+++ Prac3/src/ru/mirea/lab3/Interfaces/Movable.java
@@ -0,0 +1,13 @@
+package ru.mirea.lab3.Interfaces;
+import java.lang.*;
+
+public interface Movable
+{
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+}
