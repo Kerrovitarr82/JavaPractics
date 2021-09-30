e24c88bbacbb3796d827e8c8ec52862ea14236f9
Kerrovitarr82 <kokosk29@gmail.com>
2021-09-30 09:39:46 +0300

    sdsd

--- Practic/src/ru/mirea/task8/Main.java
+++ Practic/src/ru/mirea/task8/Main.java
@@ -6,39 +6,27 @@ public class Main extends Animation
 {
     public static void main(String[] args) {
         new Animation();
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/0.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/1.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/2.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/3.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/4.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/5.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/6.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/7.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/8.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/9.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/10.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/11.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/12.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/13.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/14.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/15.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/16.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/17.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/18.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/19.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/20.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/21.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/22.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/23.gif").getImage());
-        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/manime/24.gif").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-0.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-1.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-2.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-3.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-4.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-5.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-6.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-7.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-8.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-9.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-10.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-11.png").getImage());
+        framlist.add(new ImageIcon("Practic/src/ru/mirea/task8/rock/rock-12.png").getImage());
         Animation animImg = new Animation();
         while (true) {
-            for (int i = 0; i < 25; i++) {
+            for (int i = 0; i < 13; i++) {
                 image = framlist.get(i);
                 animImg.repaint();
                 try
                 {
-                    Thread.sleep(15);
+                    Thread.sleep(30);
                 }
                 catch (InterruptedException e)
                 {
